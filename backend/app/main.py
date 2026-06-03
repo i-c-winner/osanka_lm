306c236616a59673766ad6f3d4d29db83d4e007e
Dmitrii <dimitriy1861@gmail.com>
2026-06-03 08:58:48 +0500

    fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,6 @@ app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.VERSION,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
-    redirect_slashes=False,
 )
 
 app.add_middleware(
